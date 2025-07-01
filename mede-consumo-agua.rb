80a499cc65d5db5a8197432835b357040c947874
Henry <henry.devmotion@gmail.com>
2025-07-01 18:08:51 -0300

    Alg-03

---
+++ mede-consumo-agua.rb
@@ -0,0 +1,25 @@
+# Dificil este exercício. Travei neste ponto.
+
+def consumo_agua(vazao_m3_h, tempo_h)
+    limite_mensal = 8000
+    limite_diario_total = limite_mensal / 30
+    limite_por_comodo = 
+
+# Exercício completo corrigido.
+
+def consumo_agua(vazao_m3_h, tempo_h, proporcao_setor)
+  limite_mensal = 8000.0
+  limite_diario_total = limite_mensal / 30
+  limite_setor = limite_diario_total * proporcao_setor
+
+  consumo_litros = vazao_m3_h * 1000 * tempo_h
+
+  puts "Consumo do setor: #{consumo_litros.round(2)} litros"
+  puts "Limite ideal para o setor: #{limite_setor.round(2)} litros"
+
+  if consumo_litros > limite_setor
+    puts "Alerta: consumo de água acima do ideal!"
+  else
+    puts "Consumo dentro do ideal!"
+  end
+end
