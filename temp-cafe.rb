616ff405b6c1dd5111b7abcfcbd2678141c481d4
Henry <163439285+HenryDevMotion@users.noreply.github.com>
2025-07-01 18:24:30 -0300

    Alg-01

--- temp-cafe.rb
+++ temp-cafe.rb
@@ -1,17 +1,18 @@
-#Problema = Acabamos de fazer café e queremos saber a temperatura ideal para toma-lo que deve ser entre 60ºC e 65ºC.
-#Porém a temperatura cai 5% por minuto e a temperatura do ambiente influencia no esfriamento em 10 minutos.
+#Problema = Acabamos de fazer café e queremos saber a temperatura ideal para toma-lo
+#que deve ser entre 60ºC e 65ºC. Porém a temperatura cai 5% por minuto e a temperatura do
+#ambiente influencia no esfriamento em 10 minutos.
 
 #O objetivo é descobrir o tempo exato em que a temperatura do café estará entre 60ºC e 65ºC
 
 #O que comecei fazendo:
 
-#temperatura = 90ºC
-#tempo_esfriamento = temperatura / 10
-#temp_ideal = temperatura % 10
+temperatura = 90ºC
+tempo_esfriamento = temperatura / 10
+temp_ideal = temperatura % 10
 
-#if temperatura >= 60 - Não dei conta de continuar
+if temperatura >= 60 - Não dei conta de continuar
 
-#puts "O tempo ideal para tomar o café será em #{temp_ideal}" - Obviamente não ia dar certo.
+puts "O tempo ideal para tomar o café será em #{temp_ideal}" - Obviamente não ia dar certo.
 
 #Eis o código correto:
 
